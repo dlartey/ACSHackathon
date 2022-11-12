a15e9780ca3fcd273e73519bbba472abbb464ffc
Daniel Lartey <58071586+dlartey@users.noreply.github.com>
2022-11-12 17:46:14 +0000

    Added Change font(still need to fix)

--- popup.js
+++ popup.js
@@ -1,2 +1,17 @@
 console.log("This is a popup")
 console.log("This is also a popup")
+
+function changeFont() {
+    let par = document.querySelectorAll("p");
+    par.forEach(element => {
+        console.log(element)
+    element.style.fontFamily  = "Times New Roman";
+    });
+}
+
+browser.tabs.executeScript({
+    code: `(${changeFont})()` //argument here is a string but function.toString() returns function's code
+}, (results) => {
+    //Here we have just the innerHTML and not DOM structure
+
+});
