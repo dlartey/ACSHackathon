02140d5203f00f61bb634d69fb73dce8d68201e8
Lanrayy <lanre.sodeinde1@gmail.com>
2022-11-12 22:17:34 +0000

    Added a form page

---
+++ app/forms.py
@@ -0,0 +1,31 @@
+from email import message
+from flask_wtf import FlaskForm
+from wtforms import IntegerField
+from wtforms import StringField
+from wtforms import TextAreaField
+from wtforms import DateField
+from wtforms import SubmitField
+from wtforms import PasswordField
+from wtforms import BooleanField
+from wtforms import ValidationError
+from wtforms import SelectField
+from wtforms import FloatField
+from wtforms import DateTimeField
+from wtforms.validators import InputRequired, EqualTo, Length, Email, Regexp, NumberRange
+from flask import flash
+from datetime import datetime, timedelta
+import calendar
+
+# sign up form
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('name', validators=[InputRequired(), Regexp(
+        "^([^0-9]*)$", message="Name cannot contain digits")])
+    #username = StringField('username', validators=[InputRequired(), Length(min=2, max=20)])
+    email = StringField('email', validators=[InputRequired()])
+    password1 = PasswordField('password1', validators=[InputRequired()])
+    password2 = PasswordField('password2', validators=[EqualTo(
+        'password1'), InputRequired()])  # makes sure password1 equals password2
+    user_type = SelectField('user_type', choices=[('default', 'default'), (
+        'senior', 'senior'), ('student', 'student')], validators=[InputRequired()])
--- app/views.py
+++ app/views.py
@@ -1,10 +1,28 @@
-from flask import render_template
+from flask import request, render_template, flash, redirect, url_for
 from app import app
+from .forms import RegisterForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = {'name': 'Sam Wilson'}
+    if request.method == 'POST':
+        try:
+            # get info on the app the was requested
+            app_requested = request.form['button']
+            print(app_requested)
+            # redirect to the appropriate app
+            return redirect(url_for(app_requested))
+        except Exception as e:
+            flash("Try again! An error occured")
+
     return render_template('index.html',
-                           title='Simple template example',
-                           user=user)
+                           title='Simple template example')
+
+
+@app.route('/form')
+def form():
+    user = {'name': 'Sam Wilson'}
+    form = RegisterForm()
+    return render_template('form.html',
+                           title='Form page',
+                           form=form)
