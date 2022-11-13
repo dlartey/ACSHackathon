013833c1b1ad903f3d88107bf312255dd6a29c71
Lanrayy <lanre.sodeinde1@gmail.com>
2022-11-13 04:19:36 +0000

    Added route to form

--- app/views.py
+++ app/views.py
@@ -19,10 +19,16 @@ def index():
                            title='Simple template example')
 
 
-@app.route('/form')
+@app.route('/form', methods=['GET', 'POST'])
 def form():
-    user = {'name': 'Sam Wilson'}
     form = RegisterForm()
+    if request.method == 'POST':
+        try:
+            return redirect(url_for('results'))  # redirect to login page
+        except Exception as e:
+            app.logger.error(e)
+            flash(e, 'alert alert-danger')
+
     # save results into session
     session['score'] = 100
     return render_template('form.html',
@@ -43,36 +49,37 @@ def about():
 
 
 self_care = ['https://www.thespruce.com/how-to-create-a-daily-routine-2648007',
-'https://www.nhs.uk/live-well/eat-well/how-to-eat-a-balanced-diet/eight-tips-for-healthy-eating/',
-'https://www.counselling-directory.org.uk/memberarticles/knowing-your-limits-what-are-boundaries-and-why-are-they-important',
-'https://www.newhealthadvisor.org/how-to-stop-using-your-phone-so-much.html']
+             'https://www.nhs.uk/live-well/eat-well/how-to-eat-a-balanced-diet/eight-tips-for-healthy-eating/',
+             'https://www.counselling-directory.org.uk/memberarticles/knowing-your-limits-what-are-boundaries-and-why-are-they-important',
+             'https://www.newhealthadvisor.org/how-to-stop-using-your-phone-so-much.html']
 
 
 pr = ['https://www.howtomakefriends.co.uk/',
-'https://selfcarefundamentals.com/how-to-improve-relationships-with-your-parents/',
-'https://professional.dce.harvard.edu/blog/eight-things-you-can-do-to-improve-your-communication-skills/',
-'https://www.healthline.com/health/mental-health/how-to-control-anger'
-]
+      'https://selfcarefundamentals.com/how-to-improve-relationships-with-your-parents/',
+      'https://professional.dce.harvard.edu/blog/eight-things-you-can-do-to-improve-your-communication-skills/',
+      'https://www.healthline.com/health/mental-health/how-to-control-anger'
+      ]
 
 emotion = ['https://www.healthline.com/health/how-to-control-your-emotions',
-'https://www.nhs.uk/mental-health/self-help/tips-and-support/cope-with-depression/',
-'https://www.healthline.com/health/how-to-stop-being-lazy',
-'https://www.nhs.uk/mental-health/self-help/tips-and-support/how-to-be-happier/']
+           'https://www.nhs.uk/mental-health/self-help/tips-and-support/cope-with-depression/',
+           'https://www.healthline.com/health/how-to-stop-being-lazy',
+           'https://www.nhs.uk/mental-health/self-help/tips-and-support/how-to-be-happier/']
 
 
-social=[
-'https://myh.org.uk/how-to-reduce-social-media-usage/',
-'https://erickimphotography.com/blog/2017/03/05/how-to-be-less-addicted-to-social-media/',
-'https://www.headspace.com/mindfulness/negative-effects-of-social-media',
-'https://www.healthline.com/health/mental-health/how-to-cope-with-anxiety'
+social = [
+    'https://myh.org.uk/how-to-reduce-social-media-usage/',
+    'https://erickimphotography.com/blog/2017/03/05/how-to-be-less-addicted-to-social-media/',
+    'https://www.headspace.com/mindfulness/negative-effects-of-social-media',
+    'https://www.healthline.com/health/mental-health/how-to-cope-with-anxiety'
 ]
 
 areas = {
-    'Social Media':social,
-    'Self Care':self_care,
-    'Personal Relationships':pr,
-    'Emotions':emotion
+    'Social Media': social,
+    'Self Care': self_care,
+    'Personal Relationships': pr,
+    'Emotions': emotion
 }
+
 
 @app.route('/results')
 def results():
