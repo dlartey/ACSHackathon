f57ac6967647014fe31e9d238bf1aa0cf09ee405
Lanrayy <lanre.sodeinde1@gmail.com>
2022-11-13 06:33:47 +0000

    Styled the form

--- app/forms.py
+++ app/forms.py
@@ -22,39 +22,52 @@ import calendar
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('name', validators=[InputRequired(), Regexp("^([^0-9]*)$", message="Name cannot contain digits")])
-    age = SelectField('age', choices=['Under 18', '18-25', '26-36', '36-49','50+'])
-    
-    screentime_a = SelectField('screentime_a', choices=['Less than 3 hours', '3-5 hours', '6-15 hours', 'Over 16 hours'])
-    influencer_a = SelectField('influencer_a', choices=['Yes - all the time', 'Yes - sometimes', 'No, never']) # have you compared yourself to another user on social media
-    filters_a = SelectField('filters_a', choices=['Yes - all the time', 'Yes - occasionally', 'Never'])
-    self_image_a = SelectField('self_image_a', choices=['Yes, I love how I look', 'Yes, but I am ocassionally self conscious', 'No, I hate it']) # like how i look
-    messages_a = SelectField('messages_a', choices=['Yes', 'No']) # do you have a lot of unread messages 
-    bedtime_a = SelectField('bedtime_a', choices=['Yes, all the time', 'Yes - sometimes', 'Never']) # use phone before bed often
-    following_a = SelectField('following_a', choices=['Family/friends', 'Celebrities','Beauty influencers', 'Fitness influencers', 'Pets/Animals']) # who do you mostly follow online
-    recommendation_a = SelectField('recommendation_a', choices=['Yes, significantly', 'Yes - occassionally', 'Never']) # does social media influence your product choices
+    name = StringField('name', validators=[InputRequired(), Regexp(
+        "^([^0-9]*)$", message="Name cannot contain digits")])
+    age = SelectField('age', choices=['Please choose one ...',
+                      'Under 18', '18-25', '26-36', '36-49', '50+'])
 
-    outside_b = SelectField('outside_b', choices=['Yes','No']) # gone outside
-    eaten_b = SelectField('eaten_b', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # have you eaten today 
-    nonline_b = SelectField('nonline_b', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # nonline recreational activity
-    exercise_b = SelectField('exercise_b', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # exercised per week 
-    sleep_b = SelectField('sleep_b', choices=['Less than 3 hours', '3-6 hours', '7+ hours'])
-    
-    relationship_c = SelectField('relationship_b', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # spoken to a friend or loved one recently
-    confide_c = SelectField('confide_c', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # have confidant
-    judged_c = SelectField('judged_c', choices=['Yes - all the time', 'Yes - occasionally', 'Never']) # feel judged by those around you
+    screentime_a = SelectField('screentime_a', choices=['Please choose one ...',
+                               'Less than 3 hours', '3-5 hours', '6-15 hours', 'Over 16 hours'])
+    # have you compared yourself to another user on social media
+    influencer_a = SelectField('influencer_a', choices=['Please choose one ...',
+                               'Yes - all the time', 'Yes - sometimes', 'No, never'])
+    filters_a = SelectField('filters_a', choices=['Please choose one ...',
+                            'Yes - all the time', 'Yes - occasionally', 'Never'])
+    self_image_a = SelectField('self_image_a', choices=['Please choose one ...',
+                               'Yes, I love how I look', 'Yes, but I am ocassionally self conscious', 'No, I hate it'])  # like how i look
+    # do you have a lot of unread messages
+    messages_a = SelectField('messages_a', choices=[
+                             'Please choose one ...', 'Yes', 'No'])
+    bedtime_a = SelectField('bedtime_a', choices=['Please choose one ...',
+                            'Yes, all the time', 'Yes - sometimes', 'Never'])  # use phone before bed often
+    following_a = SelectField('following_a', choices=['Please choose one ...',
+                              'Family/friends', 'Celebrities', 'Beauty influencers', 'Fitness influencers', 'Pets/Animals'])  # who do you mostly follow online
+    # does social media influence your product choices
+    recommendation_a = SelectField('recommendation_a', choices=['Please choose one ...',
+                                   'Yes, significantly', 'Yes - occassionally', 'Never'])
 
+    outside_b = SelectField('outside_b', choices=[
+                            'Please choose one ...', 'Yes', 'No'])  # gone outside
+    eaten_b = SelectField('eaten_b', choices=['Please choose one ...',
+                          'Yes - all the time', 'Yes - occasionally', 'No'])  # have you eaten today
+    nonline_b = SelectField('nonline_b', choices=['Please choose one ...',
+                            'Yes - all the time', 'Yes - occasionally', 'No'])  # nonline recreational activity
+    exercise_b = SelectField('exercise_b', choices=['Please choose one ...',
+                             'Yes - all the time', 'Yes - occasionally', 'No'])  # exercised per week
+    sleep_b = SelectField('sleep_b', choices=['Please choose one ...',
+                          'Less than 3 hours', '3-6 hours', '7+ hours'])
 
-    stress_d = SelectField('stress_d', choices=['Yes - all the time', 'Yes - occasionally', 'Never']) # get stressed easy?
-    concentrate_d = SelectField('concentrate_d', choices=['Yes - all the time', 'Yes - occasionally', 'No']) # good at concentrating
-    depress_d = SelectField('depress_d', choices=['Yes - all the time', 'Yes - occasionally', 'Never'])
+    relationship_c = SelectField('relationship_b', choices=['Please choose one ...',
+                                 'Yes - all the time', 'Yes - occasionally', 'No'])  # spoken to a friend or loved one recently
+    confide_c = SelectField('confide_c', choices=['Please choose one ...',
+                            'Yes - all the time', 'Yes - occasionally', 'No'])  # have confidant
+    judged_c = SelectField('judged_c', choices=['Please choose one ...',
+                           'Yes - all the time', 'Yes - occasionally', 'Never'])  # feel judged by those around you
 
-
-
-    
-
-
-
-
-
-    
+    stress_d = SelectField('stress_d', choices=['Please choose one ...',
+                           'Yes - all the time', 'Yes - occasionally', 'Never'])  # get stressed easy?
+    concentrate_d = SelectField('concentrate_d', choices=['Please choose one ...',
+                                'Yes - all the time', 'Yes - occasionally', 'No'])  # good at concentrating
+    depress_d = SelectField('depress_d', choices=['Please choose one ...',
+                            'Yes - all the time', 'Yes - occasionally', 'Never'])
